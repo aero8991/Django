e050dd8da10a7459495b47748bb15cbe7d9ab598
Dan Rossano <dannyboy.rossano@gmail.com>
2021-12-11 00:32:57 -0800

    updated app for heroku

--- painter/settings.py
+++ painter/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,6 +53,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
+
+
 ]
 
 ROOT_URLCONF = 'painter.urls'
@@ -127,6 +134,8 @@ STATICFILES_DIRS = [
 
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedMainfestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -148,3 +157,4 @@ DEFAULT_FROM_EMAIL = 'daniel.rossano10@gmail.com'
 
 #EMAIL_USE_SSL = False
 
+django_heroku.settings(locals())
